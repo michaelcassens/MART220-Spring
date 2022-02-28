444d141bd340db620889c40dca9f8ed744b46dcc
Michael Cassens <michaelcassens@gmail.com>
2022-02-28 12:58:41 -0700

    updated week 7

---
+++ Week 7/js/imageclass.js
@@ -0,0 +1,48 @@
+class imageclass
+{
+    constructor(img, x, y, w, h)
+    {
+        this.img = img;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    getImage()
+    {
+        return this.img;
+    }
+    getX()
+    {
+        return this.x;
+    }
+    getY()
+    {
+        return this.y;
+    }
+    getW()
+    {
+        return this.w;
+    }
+    getH()
+    {
+        return this.h;
+    }
+
+    moveX(speed)
+    {
+        this.x += speed;
+    }
+
+    draw()
+    {
+        image(this.img,
+	    this.x, 
+	    this.y, 
+	    this.w,
+	    this.h);	
+    }
+
+    
+}
---
+++ Week 7/js/sketch.js
@@ -0,0 +1,19 @@
+
+function preload()
+{
+	// read in the file
+
+}
+
+function setup()
+{
+	createCanvas(displayWidth, displayHeight);
+	
+	
+
+}
+
+function draw(){
+	background(240);
+
+}
