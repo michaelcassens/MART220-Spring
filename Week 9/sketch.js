715f310f7fda2016a104401e02c8f4460d9dfb67
Michael Cassens <michaelcassens@gmail.com>
2022-03-14 14:25:21 -0700

    updated the sketch to include 3D

--- Week 9/sketch.js
+++ Week 9/sketch.js
@@ -1,24 +1,51 @@
-const particles = [];
+let img;
+let pg;
 
-function setup() {
-  createCanvas(600, 600);
+//var degrees = 1;
+//var translate = 1;
+var duh = 1;
+
+function preload(){
+  img = loadImage("taco.png");
+ // pg = createGraphics(156,156);
 }
-
-function draw() {
+function setup(){
+  createCanvas(500,500,WEBGL);
+}
+function draw(){
   background(0);
+//console.log(duh);
+ // rotateX(duh * .01);
+ // rotateY(duh * .01);
+ // texture(img);
+ // box(85);
+ // duh++;
+ //rotateX(frameCount * 0.01);
+ //rotateY(frameCount * 0.01);
+ //rotateZ(frameCount * 0.01);
+ //cylinder(20, 50,2);
+ //rotateX(frameCount * 0.01);
+ //rotateY(frameCount * 0.01);
+ //torus(30, 15);
 
-  for (let i = 0; i < 5; i++) {
-    let p = new Particle();
-    particles.push(p);
-  }
-  for (let i = particles.length - 1; i >= 0; i--) {
-    particles[i].update();
-    particles[i].show();
+normalMaterial();
+push();
+translate(-100,-100);
+rotateX(frameCount * 0.01);
+rotateY(frameCount * 0.01);
+box(150, 150, 150);
+pop();
 
-    if (particles[i].finished()) {
-      // remove this particle
-      particles.splice(i, 1);
-    }
-  }
+push();
+translate(100,100);
+rotateX(frameCount * 0.01);
+rotateY(frameCount * 0.01);
+torus(50, 15, 24, 24);
+pop();
+
+
+
+
+
 
 }
