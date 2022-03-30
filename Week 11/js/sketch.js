f738706883c6bfc0ad79490023be0497034e38fb
Michael Cassens <michaelcassens@gmail.com>
2022-03-30 12:59:56 -0700

    Update sketch.js

--- Week 11/js/sketch.js
+++ Week 11/js/sketch.js
@@ -1,16 +1,21 @@
 let myModel;
 let myTexture;
+var rotateSpeed = .01;
+var spiderX = 10;
+var spiderY = 10;
+
+var spiderX2 = 200;
+var spiderY2 = 200;
+
 function preload()
 {
     myModel = loadModel("./assets/spider.obj");
-    myTexture = loadImage("./assets/fractal.jpg");
+    myTexture = loadImage("./assets/grass.jpg");
 }
 // this function is called only once
 function setup()
 {
-
-    createCanvas(800,600, WEBGL);
-
+    createCanvas(1080,768, WEBGL);
 }
 /* this function is called continuously
     while the sketch is open in the browser
@@ -18,14 +23,48 @@ function setup()
 function draw()
 {
     background(20);
+push();
+    translate(spiderX,spiderY,-500);
     scale(2); // Scaled to make model fit into canvas
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
+    rotateX(frameCount * rotateSpeed);
+    rotateY(frameCount * rotateSpeed);
     normalMaterial(); // For effect
     smooth();
     noStroke();
     texture(myTexture);
     // this displays the model using the "model" function
     model(myModel,24);
-    
+pop();
+push();
+    translate(spiderX2,spiderY2,-500);
+    scale(2); // Scaled to make model fit into canvas
+    rotateX(frameCount * rotateSpeed);
+    rotateY(frameCount * rotateSpeed);
+    normalMaterial(); // For effect
+    smooth();
+    noStroke();
+    texture(myTexture);
+    // this displays the model using the "model" function
+    model(myModel,24);
+   pop(); 
 }
+
+function mousePressed()
+{
+    rotateSpeed += .5;
+    console.log(rotateSpeed);
+}
+
+function mouseReleased()
+{
+    rotateSpeed -= .5;
+}
+
+function mouseDragged()
+{
+    spiderX = mouseX-width/2;
+    spiderY = mouseY-height/2;
+    console.log(spiderX);
+}
+
+
